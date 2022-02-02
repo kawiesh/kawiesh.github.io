dafe2efcf2ca918b984abddf61b971a03e16d674
Mysterious <83178906+nostalgickd@users.noreply.github.com>
2022-02-02 12:02:32 -0300

    Update quickcopy.js

--- bookmarklets/quickcopy.js
+++ bookmarklets/quickcopy.js
@@ -2,41 +2,105 @@ let create= (x)=> document.createElement(x),
 select= (x,y=document)=> y.querySelector(x),
 selectAll= (x,y=document)=> y.querySelectorAll(x);
 
-let popup= create("div");
-popup.style.cssText="display:flex; padding: 5px; height: 50px; width: 100px; background: white; border: 1px solid black; position: fixed; top:0; right:0; justify-content:space-between; display: none;";
+let template= create("template");
+template.innerHTML=`<style>
+*{
+box-sizing:border-box; 
+margin:0; padding:0; 
+outline:none;
+pointer-events: auto;
+}
 
+#container{
+z-index:99999;
+/*width: 500px; height: 400px;*/
+width: 80vw;
+aspect-ratio: 1/1.5;
+position: absolute;
+left:20px; top:20px;
+display:flex;
+flex-direction:column;
+pointer-events: none;
+}
 
-let selection="";
+path{
+stroke:red;
+stroke-width:5px;
+}
+	
+.close{
+position:absolute;
+top:-20px; left: 0;
+width:20px; height:20px;
+border:1px solid red;
+cursor: pointer;
+}
 
-let copy= create("button");
-copy.innerHTML= "Copy";
-let close= create("button");
-close.innerHTML= "Close";
+textarea{
+padding: 5px;
+width:100%; height:100%;
+font: bold 10px "Courier New";
+white-space: nowrap;
+}
 
-copy.onclick=()=>{
-try{document.execCommand("copy")}
-catch(x){navigator.clipboard.writeText(getSelection())}
+</style>
+<div id="container">
+<svg class="close" viewbox="0 0 40 40"><path d="M 10,10 L 30,30 M 30,10 L 10,30"/></svg>
+<textarea></textarea>
+</div>`;
+
+let name= "kd-copy"
+if (!customElements.get(name)){
+window.customElements.define(name,
+class extends HTMLElement{
+constructor(){
+super();
+this.attachShadow({mode:"open"});				
+}			
+})
+}
+
+//-------------------------------------------- 
+let a= create(name), b= select(name);
+a.shadowRoot.append(template.content.cloneNode(true));
+
+if(b){
+b.remove();
+document.body.append(a);
+}
+else document.body.append(a);
+
+let container= select("#container", a.shadowRoot),
+all= selectAll("#container>*", a.shadowRoot),
+close= select(".close", a.shadowRoot),
+ta= select("textarea", a.shadowRoot);
+
+//----
+document.body.ontouchstart= (e)=>{
+if(e.target!=a){
+ta.value= e.target.innerText;
 };
 
-
-close.onclick=()=>{
-let a= getSelection();
-popup.style.display= "none";
-a.removeAllRanges();
+close.onclick= function(){
+this.classList.toggle("hidden");
+let hide= this.classList.contains("hidden");
+all.forEach(i=> i.style.display= hide ? "none" : "block");
+this.style.display= "block";
 };
 
-popup.append(copy,close);
+function drag(e){
+e.preventDefault();
+let touchLocation = e.targetTouches[0];
+let x= touchLocation.pageX || e.pageX;
+let y= touchLocation.pageY || e.pageY;
 
-document.body.append(popup);
+container.style.left= x + "px";
+container.style.top= y + "px";
+};
 
-
-document.body.ondblclick=(e)=>{
-
-let a= document.createRange();
-a.selectNodeContents(e.target);
-let b= getSelection();
-
-b.removeAllRanges(); b.addRange(a);
-popup.style.display= "flex";
-
+close.ontouchmove= drag;
+close.onmousemove= drag;
+close.ondblclick=()=>{
+a.remove();
+document.body.ontouchstart=()=> null;
 };
