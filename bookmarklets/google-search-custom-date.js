8b6b1f5dfee457eadfbcf47e69ec3be54d02fd23
Kawiesh <kawieshd@gmail.com>
2022-02-14 21:05:56 -0300

    Update google-search-custom-date.js

--- bookmarklets/google-search-custom-date.js
+++ bookmarklets/google-search-custom-date.js
@@ -1,8 +1,4 @@
-let a= location.hostname+'/search?q=';
-if (!a.includes('www.google')) alert ('Not on Google');
-else {
-b= document.forms[0].elements['q'].value,
-c= new Date(),
+let c= new Date(),
 d= c.getFullYear(),
 e= c.getMonth()+1,
 f= c.getDate(),
@@ -10,7 +6,13 @@ g= e+'/'+f+'/'+d,
 h= prompt('Enter start date (Month/Day/Year)','01/01/2000'),
 i= prompt('Enter end date (Month/Day/Year)',g),
 j= '&tbs=cdr:1,cd_min:'+h+',cd_max:'+i,
+
+if (!location.hostname.includes('google')){
+let b= prompt("Enter Keywords to google");
+location= 'https://google.com/search?q='+b+j;
+}
+else {
+let b= document.forms[0].elements['q'].value,
 k= location.protocol+'//';
- 
-location= k+a+b+j;
+location= k+location.hostname+'search?q='+b+j;
 }
