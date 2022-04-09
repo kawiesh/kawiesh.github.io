cc2c0ddd2996dd07b25b04c5d787cefa52301a5b
Mysterious <83178906+nostalgickd@users.noreply.github.com>
2022-04-09 14:16:37 -0300

    .

---
+++ bookmarklets/sleep-timer.js
@@ -0,0 +1,56 @@
+let a= prompt("Close tab after how many minutes?");
+let b= document.querySelector("html");
+if(a){
+let c= window.open(location.href);
+document.title= "Sleep Timer";
+let d= a*60*1000;
+let e= d;
+doit(e,b,d);
+
+setTimeout(()=>{
+c.close();
+b.innerHTML= `>><b>New tab opened.<br>
+>>!</b>`;
+},d);
+}
+
+
+function formatTime(x){
+let hms= new Date(x).toISOString().substr(11,8).split(":");
+let h= Number(hms[0]), m= Number(hms[1]), s= Number(hms[2]);
+let H,M;
+
+switch(h){
+case 0: H= ""; break;
+case 1: H= "1 hour, "; break;
+default:H= `${h} hours, `;
+}
+
+switch(m){
+case 0: M= ""; break;
+case 1: M= "1 min. & "; break;
+default:M= `${m} min. & `;
+}
+
+
+return `${H}${M}${s} sec.`;
+
+}
+
+
+
+
+function doit(x,y,z){
+if(x>0){
+
+y.innerHTML= `>><b>New tab opened.<br>
+>>It'll close after ${formatTime(x)}`;
+x= x-1000;
+setTimeout(()=>{
+doit(x,y,z)
+},1000);
+
+}
+else y.innerHTML= `>><b>New tab opened.<br>
+>>Closed after ${formatTime(z)}`;
+}
